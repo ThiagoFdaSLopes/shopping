374136da3f5eda523bd4e0e2746c2a36acc7f4d5
Thiago Lopes <programadorthiagolopes@gmail.com>
2023-09-09 12:42:47 -0300

    feat: Mock items shopping list

---
+++ web/src/Mocks/items.ts
@@ -0,0 +1,47 @@
+import hamburger from "../Images/hamburger.png";
+import acerolajuice from "../Images/acerolajuice.png";
+import chocolate from "../Images/chocolate.png";
+import cookies from "../Images/cookies.png";
+import cupcake from "../Images/cupcake.png";
+import grapejuice from "../Images/grapejuice.png";
+import hamburger2 from "../Images/hamburger2.png";
+
+const items = [
+  {
+    name: "X - Tudo",
+    price: "100",
+    image: hamburger,
+  },
+  {
+    name: "Grape Juice",
+    price: "100",
+    image: grapejuice,
+  },
+  {
+    name: "Acerola Juice",
+    price: "100",
+    image: acerolajuice,
+  },
+  {
+    name: "Cookies",
+    price: "100",
+    image: cookies,
+  },
+  {
+    name: "Chocolate",
+    price: "100",
+    image: chocolate,
+  },
+  {
+    name: "Cupcake",
+    price: "100",
+    image: cupcake,
+  },
+  {
+    name: "X - Salada",
+    price: "100",
+    image: hamburger2,
+  },
+];
+
+export default items;
